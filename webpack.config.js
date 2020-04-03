61244ab57a5c7cb0cee866c4d3a9a5bb6511c88f
Dave <dave.gagnat@gmail.com>
2020-04-03 15:44:38 -0500

    Added webpack file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              '@babel/env'
+            ]
+          }
+        },
+      }
+    ]
+  }
+};
