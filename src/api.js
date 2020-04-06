aff14a7e3e64358dd8606004d59bb11b20f7dcb5
Dave <dave.gagnat@gmail.com>
2020-04-06 11:13:18 -0500

    Commented out code and explored line chart

--- src/api.js
+++ src/api.js
@@ -14,48 +14,8 @@ import axios from 'axios';
 
 // bitcoin(); 
 
-var CancelToken = axios.CancelToken;
-var source = CancelToken.source();
 
-export const cancelBitcoin = () => {
-
-  axios.get('https://cloud-sse.iexapis.com/stable/cryptoQuotes?symbols=BTCUSDT&token=pk_9dbe2686af9e4869b46f1ff7fb1b54dd', {
-    cancelToken: source.token
-  }).catch(function (thrown) {
-    if (axios.isCancel(thrown)) {
-      console.log('Request canceled', thrown.message);
-    } else {
-      // handle error
-    }
-  });
-
-  axios.post('https://cloud-sse.iexapis.com/stable/cryptoQuotes?symbols=BTCUSDT&token=pk_9dbe2686af9e4869b46f1ff7fb1b54dd', {
-    name: 'new name'
-  }, {
-    cancelToken: source.token
-  })
-  // cancel the request (the message parameter is optional)
-  source.cancel('Operation canceled by the user.');
-}
-  
-var cancel;
-
-export const cancelBitcoinToken = () => {
-
-  axios.get('https://cloud-sse.iexapis.com/stable/cryptoQuotes?symbols=BTCUSDT&token=pk_9dbe2686af9e4869b46f1ff7fb1b54dd', {
-      cancelToken: new CancelToken(function executor(c) {
-        // An executor function receives a cancel function as a parameter
-        cancel = c;
-      })
-    });
-
-  // cancel the request
-  cancel();
-
-}
-
-
-
+// Testing AXIOS
 // export const test = async () => {
 //   try {
 //     const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
@@ -66,39 +26,17 @@ export const cancelBitcoinToken = () => {
 // }; 
 
 
-export const bitcoin = () => {
-  axios.get('https://cloud-sse.iexapis.com/stable/cryptoQuotes?symbols=BTCUSDT&token=pk_9dbe2686af9e4869b46f1ff7fb1b54dd')
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .then(() => {
+// export const bitcoin = () => {
+//   axios.get('https://cloud-sse.iexapis.com/stable/cryptoQuotes?symbols=BTCUSDT&token=pk_9dbe2686af9e4869b46f1ff7fb1b54dd')
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+//     .then(() => {
 
-    });
-}
-
-
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
+//     });
 // }
 
-// // Make a request for a user with a given ID
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });
+
