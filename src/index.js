37125c14870f991900b3d3906b527cbb2efd16f1
Dave <dave.gagnat@gmail.com>
2020-04-06 14:41:31 -0500

    Got css to work by adding classes to elements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
+import './style.css'
 import { test, bitcoin, cancelBitcoin, cancelBitcoinToken } from './api'; 
 
 
 function component() {
   const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('title'); 
+    element.classList.add('selections-wrapper')
 
   return element;
 }
