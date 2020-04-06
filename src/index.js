cfa2040b54cb64ca034747161e3b31297bb197e8
Dave <dave.gagnat@gmail.com>
2020-04-06 14:54:31 -0500

    Added more styling classes vis classList.add to my main element.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import './style.css'
 import { test, bitcoin, cancelBitcoin, cancelBitcoinToken } from './api'; 
 
 
-function component() {
+const component = () => {
   const element = document.createElement('div');
 
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('title'); 
-    element.classList.add('selections-wrapper')
+    element.classList.add('selections-wrapper');
+    element.classList.add('chart-wrapper');
+    element.classList.add('chart');
+    element.classList.add('crypto-table-wrapper');
+    element.classList.add('crypto-table');
 
   return element;
 }
