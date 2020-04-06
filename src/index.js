69f2d7be380f8641250bb0f4bd7f6b1461b1101b
Dave <dave.gagnat@gmail.com>
2020-04-06 15:14:58 -0500

    Properly imported css file and used classes to style divs

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import './style.css'
 import { test, bitcoin, cancelBitcoin, cancelBitcoinToken } from './api'; 
 
 
-const component = () => {
-  const element = document.createElement('div');
+// const component = () => {
+//   const element = document.getElementsByClassName('root');
 
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('title'); 
-    element.classList.add('selections-wrapper');
-    element.classList.add('chart-wrapper');
-    element.classList.add('chart');
-    element.classList.add('crypto-table-wrapper');
-    element.classList.add('crypto-table');
-    element.classList.add('crypto-title');
+//     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     // element.classList.add('title'); 
+//     // element.classList.add('selections-wrapper');
+//     // element.classList.add('chart-wrapper');
+//     // element.classList.add('chart');
+//     // element.classList.add('crypto-table-wrapper');
+//     // element.classList.add('crypto-table');
+//     // element.classList.add('crypto-title');
 
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
 
 // bitcoin()
 // abortController(); 
