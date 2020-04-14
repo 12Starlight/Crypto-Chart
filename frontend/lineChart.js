0ba944b8e5494693b23d5d8bf73d56c337593020
Dave <dave.gagnat@gmail.com>
2020-04-13 23:33:05 -0500

    Figured out chart was black due to overlay class not being styled correctly

--- frontend/lineChart.js
+++ frontend/lineChart.js
@@ -127,33 +127,33 @@ class LineChart {
       .attr('x2', vis.width); 
 
     focus.append('circle')
-      .attr('r', 4);
+      .attr('r', 5);
 
     focus.append('text') 
       .attr('x', 15)
       .attr('dy', '.31em');
 
-    // vis.svg.append('rect')
-    //   .attr('transform', 'translate(' + vis.margin.left + ', ' + vis.margin.top + ')')
-    //   .attr('class', 'overlay')
-    //   .attr('width', vis.width)
-    //   .attr('height', vis.height)
-    //   .on("mouseover", () => focus.style("display", null))
-    //   .on("mouseout", () => focus.style("display", "none"))
-    //   .on("mousemove", mousemove); 
+    vis.svg.append('rect')
+      .attr('transform', 'translate(' + vis.margin.left + ', ' + vis.margin.top + ')')
+      .attr('class', 'overlay')
+      .attr('width', vis.width)
+      .attr('height', vis.height)
+      .onmouseover = () => focus.style('display', null)
+      .onmouseout = () => focus.style('display', "none")
+      .onmousemove = mousemove(); 
 
-    // function mousemove() {
-    //   let x0 = vis.x.invert(d3.mouse(this)[0]),
-    //     i = vis.bisectDate(vis.dataFiltered, x0, 1),
-    //     d0 = vis.dataFiltered[i - 1],
-    //     d1 = vis.dataFiltered[i],
-    //     d = (d1 && d0) ? (x0 - d0.date > d1.date - x0 ? d1 : d0) : 0;
-    //   focus.attr("transform", "translate(" + vis.x(d.date) + "," + vis.y(d[vis.yVariable]) + ")");
-    //   focus.select("text").text(() => d3.format("$,")(d[vis.yVariable].toFixed(2)));
-    //   focus.select(".x-hover-line").attr("y2", vis.height - vis.y(d[vis.yVariable]));
-    //   focus.select(".y-hover-line").attr("x2", -vis.x(d.date));
-    //   debugger;
-    // }
+    function mousemove() {
+      let x0 = vis.x.invert(d3.mouse(this)[0]),
+        i = vis.bisectDate(vis.dataFiltered, x0, 1),
+        d0 = vis.dataFiltered[i - 1],
+        d1 = vis.dataFiltered[i],
+        d = (d1 && d0) ? (x0 - d0.date > d1.date - x0 ? d1 : d0) : 0;
+      focus.attr("transform", "translate(" + vis.x(d.date) + "," + vis.y(d[vis.yVariable]) + ")");
+      focus.select("text").text(() => d3.format("$,")(d[vis.yVariable].toFixed(2)));
+      focus.select(".x-hover-line").attr("y2", vis.height - vis.y(d[vis.yVariable]));
+      focus.select(".y-hover-line").attr("x2", -vis.x(d.date));
+      debugger;
+    }
 
     // Update y-axis yLabel
     let newLabel = (vis.yVariable === 'price_usd') ? 'Price (USD)' :
